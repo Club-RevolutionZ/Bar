1865ebfef46ad251f8fb15f2c410253e64b25963
NexusRevZ <nexus_421@hotmail.com>
2014-09-07 18:40:31 +0800

    Update BarTender.js

--- BarTender.js
+++ BarTender.js
@@ -129,7 +129,7 @@
         status: false,
         name: "basicBot",
         loggedInID: null,
-        scriptLink: "https://rawgit.com/Yemasthui/basicBot/master/basicBot.js",
+        scriptLink: "https://rawgit.com/Club-RevolutionZ/Bar/master/BarTender.js",
         cmdLink: "http://git.io/245Ppg",
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
         chat: null,
@@ -165,9 +165,9 @@
             ],
             afkpositionCheck: 15,
             afkRankCheck: "ambassador",
-            motdEnabled: false,
-            motdInterval: 5,
-            motd: "Temporary Message of the Day",
+            motdEnabled: true, //false
+            motdInterval: 1, //5
+            motd: "Tay",
             filterChat: true,
             etaRestriction: false,
             welcome: true,
