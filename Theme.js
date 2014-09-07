d2bd2aecd4d4466a6b9a3e214f45eee7499e5618
NexusRevZ <nexus_421@hotmail.com>
2014-09-08 02:25:01 +0800

    Update Theme.js

--- Theme.js
+++ Theme.js
@@ -1,5 +1,5 @@
 /* Change room-background */
-$('.room-background').css({background: 'url("/_/static/images/community/custom/tastycat/background.3eea43d.jpg") no-repeat scroll 0% 0% transparent'});
+$('.room-background').css({background: 'url("http://i.minus.com/iZWSKjowqYKLP.gif") no-repeat scroll 0% 0% transparent'});
 /* Change video-frame */
 $('img[src*="videoframe"]').remove();
 $('.background').prepend('<img id="videoframe" src="/_/static/images/community/custom/tastycat/videoframe.cd9b648.png" style="width: 718px; height: 245px; left: -117px;" />');
