607578c761f54ec3494e09f81aefc352c8f40040
NexusRevZ <nexus_421@hotmail.com>
2014-09-09 18:06:10 +0800

    Update Beer_Google.js

--- Beer_Google.js
+++ Beer_Google.js
@@ -2,6 +2,6 @@
 $('.room-background').css({background: 'url("https://imagizer.imageshack.us/v2/1600x900q90/674/uPSdVk.png") no-repeat scroll 0% 0% transparent'});
 /* Change video-frame */
 $('img[src*="videoframe"]').remove();
-$('.background').prepend('<img id="videoframe" src="https://imagizer.imageshack.us/v2/842x287q90/540/wXOU5T.png" style="width: 718px; height: 245px; left: -117px;" />');
+$('.background').prepend('<img id="videoframe" src="https://imagizer.imageshack.us/v2/842x287q90/674/umcTkM.png" style="width: 718px; height: 245px; left: -117px;" />');
  
 //  Bookmark:  javascript:(function(){$.getScript('https://pastebin.com/raw.php?i=aUzJi8gN');}());
