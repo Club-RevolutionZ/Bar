a0d21a77c15db04b431130456a1ab2220caae9ad
NexusRevZ <nexus_421@hotmail.com>
2014-09-09 18:09:01 +0800

    Update Beer_Google.js

--- Beer_Google.js
+++ Beer_Google.js
@@ -1,6 +1,6 @@
-/* Change room-background */
+// Room background
 $('.room-background').css({background: 'url("https://imagizer.imageshack.us/v2/1600x900q90/674/uPSdVk.png") no-repeat scroll 0% 0% transparent'});
-/* Change video-frame */
+// Video frame
 $('img[src*="videoframe"]').remove();
 $('.background').prepend('<img id="videoframe" src="https://imagizer.imageshack.us/v2/842x287q90/674/umcTkM.png" style="width: 718px; height: 245px; left: -117px;" />');
  
