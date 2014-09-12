6beb923c233bbd6779a2fffeeb2d3e04bf061f92
NexusRevZ <nexus_421@hotmail.com>
2014-09-12 14:29:30 +0800

    Update BareTender.js

--- BareTender.js
+++ BareTender.js
@@ -123,7 +123,7 @@
     var botCreatorIDs = ["3885577"];
 
     var basicBot = {
-        version: "1.59 Choct",// 
+        version: "1.6 Fight Club",// 
         status: true, //false
         name: "BarTender",
         loggedInID: null,
@@ -1932,6 +1932,45 @@
                             }
                             else {
                                 return API.sendChat(subChat(basicBot.chat.drink, {nameto: user.username, namefrom: chat.un, drink: this.getDrink()}));
+                            }
+                        }
+                    }
+                }
+            },
+            
+            punishCommand: {
+                command: 'punish',
+                rank: 'residentdj',
+                type: 'startsWith',
+                punishs: ['HAI-YAH!!!!',
+                    'http://gifstumblr.com/images/trying-to-kick-down-a-door-in-real-life_1425.gif'
+                ],
+                getPunish: function () {
+                    var c = Math.floor(Math.random() * this.punishs.length);
+                    return this.punishs[c];
+                },
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.eatpunish);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.nouserpunish, {name: name}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selfpunish, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(basicBot.chat.punish, {nameto: user.username, namefrom: chat.un, punish: this.getPunish()}));
                             }
                         }
                     }
