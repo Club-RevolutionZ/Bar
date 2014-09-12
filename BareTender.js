9c9d70a06cd2ea6d2ed475ff7843d8d56c027f57
NexusRevZ <nexus_421@hotmail.com>
2014-09-12 22:30:20 +0800

    Update BareTender.js

--- BareTender.js
+++ BareTender.js
@@ -2210,15 +2210,19 @@
                     }
                 }
             },
-            
             byeCommand: {
                 command: 'bye',
                 rank: 'user',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
                         var msg = chat.message;
                         if (msg.length <= cmd.length + 1) return API.sendChat('/me ' + basicBot.settings.byeLink);
                         var argument = msg.substring(cmd.length + 1);
+                        
+                    }
                 }
             },
             
