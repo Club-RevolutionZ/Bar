76e1067c2195ffac685c84cc7a94fdc34057b3d8
NexusRevZ <nexus_421@hotmail.com>
2014-09-12 22:23:26 +0800

    Update BareTender.js

--- BareTender.js
+++ BareTender.js
@@ -170,6 +170,8 @@
             etaRestriction: false,
             welcome: true,
             opLink: null,
+            hiLink: " http://s2.favim.com/orig/36/stitch-Favim.com-298022.gif "
+            byeLink: " http://25.media.tumblr.com/tumblr_m6xj6f6qak1raa31eo1_500.gif "
             rulesLink:" :hammer: http://tinyurl.com/ClubRevolutionZRules",
             themeLink: " :tada: http://tinyurl.com/ClubRevolutionZCurrentEvent",
             fbLink: "https://www.facebook.com/ClubRevolutionZ",
@@ -2183,7 +2185,7 @@
                 rank: 'user',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
-                    if (this.type === 'startsWith' && chat.message.length !== cmd.length) return void (0);
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
                         var msg = chat.message;
