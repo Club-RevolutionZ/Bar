f0052e771c7c26a74f5287c92855d62ed498d886
NexusRevZ <nexus_421@hotmail.com>
2014-09-13 19:04:59 +0800

    For Chilly Hour 1
    For Chilly Hour 1

--- Beer_Google.js
+++ Beer_Google.js
@@ -1,7 +1,7 @@
 // Room background
-$('.room-background').css({background: 'url("https://imagizer.imageshack.us/v2/1600x900q90/674/uPSdVk.png") no-repeat scroll 0% 0% transparent'});
+$('.room-background').css({background: 'url("http://imagizer.imageshack.us/a/img633/1660/OJtuBd.gif") no-repeat scroll 0% 0% transparent'});
 // Video frame
 $('img[src*="videoframe"]').remove();
-$('.background').prepend('<img id="videoframe" src="https://imagizer.imageshack.us/v2/842x287q90/674/umcTkM.png" style="width: 718px; height: 245px; left: -117px;" />');
+$('.background').prepend('<img id="videoframe" src="https://imagizer.imageshack.us/v2/770x262q90/540/TAaqAF.png" style="width: 718px; height: 245px; left: -117px;" />');
  
 //  Bookmark:  javascript:(function(){$.getScript('https://pastebin.com/raw.php?i=aUzJi8gN');}());
