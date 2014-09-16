a1220c5f39b207cf7d69219f14c15ae6ec7ded8f
NexusRevZ <nexus_421@hotmail.com>
2014-09-16 14:28:07 +0800

    Update BareTender.js

--- BareTender.js
+++ BareTender.js
@@ -1435,17 +1435,17 @@
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
-                    else {
+                    else  {
                         var msg = chat.message;
 
                         var space = msg.indexOf(' ');
                         if (space === -1) {
-                           API.sendChat(basicBot.chat.eatcookie);
+                            API.sendChat(basicBot.chat.eatcookie);
                             return false;
                         }
                         else {
-                            var name = msg.substring(space + 2);		                            
--                            var user = basicBot.userUtilities.lookupUserName(name);
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
                             if (user === false || !user.inRoom) {
                                 return API.sendChat(subChat(basicBot.chat.nousercookie, {name: name}));
                             }
